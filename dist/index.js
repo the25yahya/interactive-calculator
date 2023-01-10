09c9efc3218807623e685e7c4a146febab8c0f53
the25yahya <114318659+the25yahya@users.noreply.github.com>
2023-01-10 18:15:45 +0100

    selected elements

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+//variables
+
+//input
+const input = document.querySelector('.input');
+
+
+
+/////////////////////////////////
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const num3 = document.getElementById('num3');
+const num4 = document.getElementById('num4');
+const num5 = document.getElementById('num5');
+const num6 = document.getElementById('num6');
+const num7 = document.getElementById('num7');
+const num8 = document.getElementById('num8');
+const num9 = document.getElementById(num9);
+const num0 = document.getElementById('num0');
+//////////////////////////////////
+   //operations variables
+   const equales = document.getAnimations('equales');
+   const minus = document.getElementsByClassName('minus');
+   const plus = document.getElementsByClassName('plus');
+   const del = document.getElementById('delete');
+   const times = document.getElementById('times');
+   const division = document.getElementById('division');
+   const c = document.getElementById('c');
+   const root = document.getElementById('root');
+   const fac = document.getElementById('fac');
+   const log = document.getElementById('log');
+   const exp = document.getElementById('exp');
+
+
+
+   //functions 
+
+   
